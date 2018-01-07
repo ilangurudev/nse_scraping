b00d076b2f4e0650f561338e2ba379357fa0280c
guru <ilangurudev@gmail.com>
2018-01-07 03:05:48 -0500

    update

--- 2_filters.R
+++ 2_filters.R
@@ -1,9 +1,15 @@
 
-filter(date > ymd(20171130)) %>% 
-  group_by(symbol, ym) %>% 
-  summarise(month_val = sum(close * volume)) %>% 
-  ungroup() %>% group_by(symbol) %>% 
-  mutate(volume_ratio = month_val/lag(month_val))  %>% 
-  filter(!is.na(volume_ratio)) %>% 
-  filter(volume_ratio > 2)
+all_data <- read_csv("all_data.csv")
 
+today_ratio <- 
+  all_data %>% 
+  filter(series == "EQ") %>% 
+  filter(date > ymd(20171130)) %>% 
+    group_by(symbol,isin, year, month) %>% 
+    summarise(month_val = sum(close * volume)) %>% 
+    ungroup() %>% group_by(symbol) %>% arrange(year, month) %>% 
+    mutate(volume_ratio = month_val/lag(month_val))  %>% 
+    filter(!is.na(volume_ratio)) %>% 
+    filter(volume_ratio > 2)
+
+write_csv(today_ratio, "today_ratio.csv")
